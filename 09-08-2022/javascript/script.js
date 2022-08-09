b662660d95124b529f96231abf0502cbf94376b4
Ravi Singh <singhravi4v@gmail.com>
2022-08-09 18:03:02 +0530

    javascript array methods

---
+++ 09-08-2022/javascript/script.js
@@ -0,0 +1,34 @@
+// var a= "ravi";
+// console.log(a);
+
+
+// POP ======================== remove from  the last
+const fruits = ["Apple","Banana","Chiku","Dragon-Fruit","Mango","Grapes"];
+// var x=fruits.pop()
+// console.log(fruits)
+// console.log(x)
+
+// 
+// splice ==================== remove from where we want and give value how much elemnt we want to remove
+// const fruits = ["Apple","Banana","Chiku","Dragon-Fruit","Mango","Grapes"];
+// x=fruits.splice(2,2,"ravi","suraj");
+// console.log(fruits);
+// console.log(x);
+
+// push ======================= adding element in last
+// fruits.push("hello");
+// console.log(fruits)
+
+// unshift ==================== add element from first
+// fruits.unshift("hello");
+// console.log(fruits);
+
+// // shift ===================== remove element from first
+// fruits.shift();
+// console.log(fruits);
+
+// concat =====================for adding array for adding into a array 
+// let a= [1,"ramu"];
+// let b= 1;
+// c= a.concat(b);
+// console.log(c);
